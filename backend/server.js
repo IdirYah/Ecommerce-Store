b726768edb993d92cbc3c941f582ccd1b364d31c
IdirYah <mi_yahiaoui@esi.dz>
2025-01-19 14:07:17 +0100

    auth with success

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js"
 
@@ -9,6 +10,9 @@ dotenv.config()
 
 const PORT = process.env.PORT || 5000
 const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
 
