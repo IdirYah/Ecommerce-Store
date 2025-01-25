cd5ac738f1389368c48d1b467c54e953b372ae3d
IdirYah <mi_yahiaoui@esi.dz>
2025-01-25 01:01:55 +0100

    payment added successfully

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
 import cartRoutes from "./routes/cart.route.js"
 import couponRoutes from "./routes/coupon.route.js"
+import paymentRoutes from "./routes/payment.route.js"
 
 import { connectDB } from "./lib/db.js"
 
@@ -21,6 +22,7 @@ app.use("/api/auth",authRoutes)
 app.use("/api/product",productRoutes)
 app.use("/api/cart",cartRoutes)
 app.use("/api/coupon",couponRoutes)
+app.use("/api/payment",paymentRoutes)
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
