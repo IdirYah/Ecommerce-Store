278f44d9db8df83a171cfd5b283002640dbe62cd
IdirYah <mi_yahiaoui@esi.dz>
2025-01-25 01:34:10 +0100

    backend finished

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import productRoutes from "./routes/product.route.js"
 import cartRoutes from "./routes/cart.route.js"
 import couponRoutes from "./routes/coupon.route.js"
 import paymentRoutes from "./routes/payment.route.js"
+import analyticRoutes from "./routes/analytics.route.js"
 
 import { connectDB } from "./lib/db.js"
 
@@ -23,6 +24,7 @@ app.use("/api/product",productRoutes)
 app.use("/api/cart",cartRoutes)
 app.use("/api/coupon",couponRoutes)
 app.use("/api/payment",paymentRoutes)
+app.use("/api/analytic",analyticRoutes)
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
